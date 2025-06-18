d544e29351bfe9a7d589e3e1c8a5fc5b3cc3fbe8
vitorgsom <vitorgabrieltitos@gmail.com>
2025-06-18 08:29:11 -0400

    Correção de variaveis de ambiente do django

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bow*6x@tv)y@e(l3r!c^9jr%c8y8ksgb-1^f%^tj@03v+xv7z='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
